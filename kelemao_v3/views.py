c64239b9072e5bc89f31d0945ec57787288e7b80
starmen1265 <starmen1265@163.com>
2021-01-24 23:55:12 +0800

    练习使用简单的模板

--- kelemao_v3/views.py
+++ kelemao_v3/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def home(request):
-    return HttpResponse("Welcome to Kelemao!")
+def index(request):
+    index_var = "第一次欢迎你"
+    index2_var ="第二次欢迎你"
+    return render(request,'index.html',{"index":index_var,"index2":index2_var})
